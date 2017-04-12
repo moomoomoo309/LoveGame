2d48e9affcb5036741013b02cc05aeec7e5e4034
Nicholas DeLello <nicholas_delello@yahoo.com>
2017-04-12 19:23:50 -0400

    Add generic code from FamiliarFaces.

--- LoveGame/src/main.lua
+++ LoveGame/src/main.lua
@@ -1,20 +1,22 @@
 --It begins.
-io.stdout:setvbuf"no" --Makes printing not buffer, so it prints instantly.
+io.stdout:setvbuf "no" --Makes printing not buffer, so it prints instantly.
 
-abstractions = require"abstractions"
-event = require"event"
+event = require "event"
 
-abstractions:generateEvents(event.pass)
-local mousePosX,mousePosY=0,0
-local w,h=50,50
-local avgColorsPerSecond=1
-event.register("mouseMoved",function(x,y) mousePosX,mousePosY=x,y end)
+local mousePosX, mousePosY = 0, 0
+local w, h = 50, 50
+local avgColorsPerSecond = 1
+
 function love.update(dt)
-	if math.random(0,1/(dt*avgColorsPerSecond))<1 then
-		abstractions.setColor(math.random(0,255),math.random(0,255),math.random(0,255))
-	end
+  if math.random(0, 1 / (dt * avgColorsPerSecond)) < 1 then
+    love.graphics.setColor(math.random(0, 255), math.random(0, 255), math.random(0, 255))
+  end
 end
 
 function love.draw()
-	abstractions.rect(mousePosX-w/2,mousePosY-h/2,w,h)
+  love.graphics.rectangle("fill",mousePosX - w / 2, mousePosY - h / 2, w, h)
+end
+
+function love.mousemoved(x, y)
+  mousePosX, mousePosY = x, y
 end
--- LoveGame/src/miscUtils.lua
+++ LoveGame/src/miscUtils.lua
@@ -14,5 +14,34 @@ function math.frandom(low, high) --Works like math.random(low,high), but returns
     return math.random(low - (high and 0 or 1), high and high - 1 or nil) + math.random()
 end
 
-return all,any
+function map(fct, numArgs, ...) --Allows functions to take unlimited arguments.
+  assert(type(fct) == "function" and type(numArgs) == "number")
+  local results = {}
+  local function innerWrap(fct, numArgs, results, args)
+    local tbl, argsLen, tblLen = {}, #args, 0 --Do only one length lookup per table
+    for i = argsLen, argsLen - numArgs + 1, -1 do
+      tblLen = tblLen + 1
+      tbl[tblLen] = args[i]
+      args[argsLen] = nil --Remove the last argument, so to avoid a table.remove() call (which is n-i, where this is O(1))
+      argsLen = argsLen - 1
+    end
+    for i = 1, tblLen / 2 do
+      tbl[i], tbl[tblLen - i + 1] = tbl[tblLen - i + 1], tbl[i]
+    end
+    if argsLen < 0 and tblLen < numArgs then
+      return results
+    end
+    results[#results + 1] = fct(unpack(tbl))
+    return #args >= numArgs and innerWrap(fct, numArgs, results, args) or results
+  end
+
+  local tbl = innerWrap(fct, numArgs, results, { ... })
+  local tblLen = #tbl
+  for i = 1, tblLen / 2 do --Reverse the table, since inserting it backwards is n(n-i) iterations, but reversing it is n/2.
+    tbl[i], tbl[tblLen - i + 1] = tbl[tblLen - i + 1], tbl[i] --Swap the first half of the elements with the last half
+  end
+  return unpack(tbl)
+end
+
+return all,any,map
 
